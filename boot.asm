70a4dee0fc1a0bc5c278b22d3d3d17e5b6e0d54a
Syed Fardin <syedfardin0612@gmail.com>
2025-10-07 22:19:37 +0530

    Hello world bootloader; Displaying letter A; Not tested

--- boot.asm
+++ boot.asm
@@ -1,3 +1,13 @@
 org 0x7c00
 bits 16
 
+start:
+    mov ah, 0x0e
+    mov al, 'A'
+    mov bx, 0
+    int 0x10
+
+    jmp $
+
+times 510-($-$$) db 0 ; If 510 bytes are not used, fill rest with 0.
+dw 0xaa55 ; boot signature
