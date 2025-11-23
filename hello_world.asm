f3bf1f99720984c767f5787d69adb89757c953fb
syedfardin <syedfardin0612@gmail.com>
2025-11-23 15:21:32 +0530

    basic hello world code written

---
+++ hello_world.asm
@@ -0,0 +1,33 @@
+org 0x7c00
+bits 16
+
+start:
+    mov si,message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+    .loop:
+        lodsb
+        cmp al,0
+        je .done
+        call print_char
+        jmp .loop
+
+
+    .done:
+        ret
+
+print_char:
+    mov ah,0x0e
+    int 0x10
+    ret
+
+
+
+
+message: db 'Hello World!', 0
+
+times 510-($-$$) db 0
+dw 0xaa55
