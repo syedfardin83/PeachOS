17d84b3177ebbd3e7814dae7556186efb5cd6529
syedfardin <syedfardin0612@gmail.com>
2026-02-11 18:57:32 +0530

    Created own interrupts by altering IVT

--- hello_world2.asm
+++ hello_world2.asm
@@ -1,6 +1,15 @@
 org 0 ; org actually sets an offset for all functions in the code
 bits 16
 
+jmp 0x7c0:start
+
+handle_zero:
+    mov bx,0
+    mov ah, 0x0e
+    mov al,'A'
+    int 0x10
+    iret
+
 start:
     cli ; Disable interrupts
     mov ax,0x7c0
@@ -10,6 +19,13 @@ start:
     mov ss,ax
     mov sp,0x7c00
     sti
+
+    ;Changing the interrupt vector table
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+
+    int 0
+
     mov si,message ; moves the address of message in si
     call print
     jmp $
