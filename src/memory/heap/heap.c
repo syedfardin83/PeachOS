d5cf5edd0a6954d2f9ef02b9c44182edf217c274
Syed Fardin <syedfardin0612@gmail.com>
2026-06-10 12:53:54 +0530

    heap: heap_malloc started

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,6 +1,7 @@
 #include "heap.h"
 #include "status.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include "memory.h"
 
 static int heap_validate_table(void* ptr, void* end, struct heap_table* table){
@@ -39,4 +40,50 @@ int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* tabl
 
 out:
     return res;  
+}
+
+static uint32_t heap_align_value_to_upper(uint32_t val){
+    if((val%PEACH_OS_BLOCK_SIZE_BYTES)==0){
+        return val;
+    }
+    val=val-(val&PEACH_OS_BLOCK_SIZE_BYTES);
+    val+=PEACH_OS_BLOCK_SIZE_BYTES;
+    return val;
+
+}
+
+int heap_find_start_block(stuct heap* heap, uint32_t total_blocks){
+    
+}
+
+void* heap_malloc_blocks(struct heap* heap, uint32_t total_blocks){
+    void* addr = 0x00;
+
+    int start_block = heap_find_start_block(heap, total_blocks);
+    if(start_block<0){
+        goto out;
+    }
+
+    addr = heap_block_to_addr(heap,start_block);
+
+    heap_mark_blocks_taken(heap, start_block, total_blocks);
+
+out:
+    return addr;
+}
+
+void* heap_malloc(struct heap* heap,size_t size){
+
+    size_t aligned_size = heap_align_value_to_upper(size);
+    uint32_t total_blocks = aligned_size/PEACH_OS_BLOCK_SIZE_BYTES;
+
+    return heap_malloc_blocks(heap, total_blocks);
+}
+
+int heap_free(struct heap* heap, void* ptr){
+    int res = 0;
+
+
+
+    return res;
 }
--- src/memory/heap/heap.h
+++ src/memory/heap/heap.h
@@ -24,6 +24,9 @@ struct heap{
 };
 
 int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* table);
+void* heap_malloc(struct heap* heap, size_t size);
+int heap_free(struct heap* heap, void* ptr);
+
 
 #endif
 
