decb0325650bdd0af4ec3626619f89efc82c2071
Syed Fardin <syedfardin0612@gmail.com>
2026-07-23 22:39:54 +0530

    fs: define file seek and mode enums in file.h

--- src/fs/file.h
+++ src/fs/file.h
@@ -1,6 +1,27 @@
 #ifndef FILE_H
 #include FILE_H
 
+#include "pparser.h"
+#include "disk.h"
+
+typedef unsigned int FILE_SEEK_MODE;
+enum{
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END
+};
+
+typedef unsigned int FILE_MODE;
+enum{
+    FILE_MODE_READ,
+    FILE_MODE_WRITE,
+    FILE_MODE_APPEND,
+    FILE_MODE_INVALID
+};
+
+void* (*FS_OPEN_FUNCTION)(struct disk* disk, struct path_part* path, FILE_MODE mode);
+int (*FS_RESOLVE_FUNCTION)(struct disk* disk);
+
 
 
 #endif
